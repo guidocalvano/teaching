84cf7c8b69a07bf14ad514736b128599d67f7928
Guido Sales Calvano <guidocalvano@guidocalvano.local>
2022-04-20 13:56:19 +0200

    using the first example

---
+++ loop.py
@@ -0,0 +1,5 @@
+from examples.single_pixel import loop as example
+
+def loop(elapsed_time, screen, mouse_x, mouse_y, mouse_is_pressed, mouse_went_down, mouse_went_up):
+
+    return example(elapsed_time, screen, mouse_x, mouse_y, mouse_is_pressed, mouse_went_down, mouse_went_up)
