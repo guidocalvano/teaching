db294b6230d821402c6d3978267960b6cf13937f
Guido Sales Calvano <guidocalvano@guidocalvano.local>
2022-04-20 13:58:05 +0200

    multiple pixels

---
+++ examples/multiple_pixels.py
@@ -0,0 +1,17 @@
+def loop(elapsed_time, screen, mouse_x, mouse_y, mouse_is_pressed, mouse_went_down, mouse_went_up):
+
+    y = 240
+
+    screen[0, y, 0] = 255
+    screen[1, y, 0] = 255
+    screen[2, y, 0] = 255
+    screen[3, y, 0] = 255
+    screen[4, y, 0] = 255
+    screen[5, y, 0] = 255
+    screen[6, y, 0] = 255
+    screen[7, y, 0] = 255
+    screen[8, y, 0] = 255
+    screen[9, y, 0] = 255
+    screen[10, y, 0] = 255
+
+    return screen
