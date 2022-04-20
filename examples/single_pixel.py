5070b50ca5fef393aa024098a35d4e44ac0ec4c9
Guido Sales Calvano <guidocalvano@guidocalvano.local>
2022-04-20 13:57:05 +0200

    single pixel example

---
+++ examples/single_pixel.py
@@ -0,0 +1,10 @@
+def loop(elapsed_time, screen, mouse_x, mouse_y, mouse_is_pressed, mouse_went_down, mouse_went_up):
+
+    x = 320
+    y = 240
+
+    screen[x, y, 0] = 255
+    screen[x, y, 1] = 0
+    screen[x, y, 2] = 0
+
+    return screen
